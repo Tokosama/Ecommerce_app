95bffc26d5d509a4f16dada08e0323bce4d133a1
Tokosama <anouartoko@gmail.com>
2026-01-11 14:09:27 +0100

    authentification implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from "express"
 import path from "path"
-
 import { clerkMiddleware } from '@clerk/express'
 
+import {serve} from "inngest/express";
+import {functions,inngest} from "./config/inngest.js";
 
 import { ENV } from "./config/env.js";
 import { connectDb } from "./config/db.js";
@@ -12,6 +13,10 @@ import { connectDb } from "./config/db.js";
 const app = express();
 
 const __dirname = path.resolve()
+
+
+app.use(express.json())
+app.use("/api/inngest",serve({client:inngest, functions:functions}))
 
 app.use(clerkMiddleware())
 
