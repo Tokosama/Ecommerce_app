7fb2e6d8021d02984ad1386ae42dd495ea83ede2
Tokosama <anouartoko@gmail.com>
2026-01-12 17:39:10 +0100

    order route and controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ import { connectDb } from "./config/db.js";
 
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(clerkMiddleware());
 app.use("/api/inngest", serve({ client: inngest, functions: functions }));
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
