1d15a615e67340244427d99fbf2e52bcbde6c892
Tokosama <anouartoko@gmail.com>
2026-01-16 00:18:21 +0100

    customers page added

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -8,6 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
 // Import your Publishable Key
+import * as Sentry from "@sentry/react";
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
@@ -16,6 +17,17 @@ if (!PUBLISHABLE_KEY) {
 
 const queryClient = new QueryClient();
 
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTR_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  enableLogs: true,
+  integrations: [Sentry.replayIntegration()],
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
